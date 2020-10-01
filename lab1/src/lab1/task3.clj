0a8d1fffe3011bbbc8cdc69d06e929bfc9b9c70a
Vlad264 <vlad-498@yandex.ru>
2020-10-02 00:31:22 +0700

    Create lab1: task3 with tests

---
+++ lab1/src/lab1/task3.clj
@@ -0,0 +1,7 @@
+(ns lab1.task3)
+
+(defn my-map [f xs]
+    (apply list (reduce (fn [ys y] (concat ys (list (f y)))) `() xs)))
+
+(defn my-filter [f xs]
+    (apply list (reduce (fn [ys y] (if (f y) (concat ys (list y)) ys)) `() xs)))
--- lab1/test/lab1/core_test.clj
+++ lab1/test/lab1/core_test.clj
@@ -18,6 +18,17 @@
     (is (= (lab1.task2/task2 `("a" "b" "c") 2) `("ab" "ac" "ba" "bc" "ca" "cb")))
     (is (= (lab1.task2/task2 `("a" "b" "c") 3) `("aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc")))))
 
+(deftest task3-test
+  (testing "tesk3"
+    (is (= (lab1.task3/my-map inc `(1 2 3)) (map inc `(1 2 3))))
+    (is (= (lab1.task3/my-map inc `(3 2 1)) (map inc `(3 2 1))))
+    (is (= (lab1.task3/my-map dec `(1 2 3)) (map dec `(1 2 3))))
+    (is (= (lab1.task3/my-map (fn [x] (* x x)) `(1 2 3)) (map (fn [x] (* x x)) `(1 2 3))))
+    (is (= (lab1.task3/my-filter even? (range 10)) (filter even? (range 10))))
+    (is (= (lab1.task3/my-filter (fn [x] (< x 5)) (range 10)) (filter (fn [x] (< x 5)) (range 10))))
+    (is (= (lab1.task3/my-filter (fn [x] (> x 5)) (range 10)) (filter (fn [x] (> x 5)) (range 10))))
+    (is (= (lab1.task3/my-filter (fn [x] (= x "a")) `("a" "b" "a")) (filter (fn [x] (= x "a")) `("a" "b" "a"))))))
+
 (deftest task4-test
   (testing "task4"
     (is (= (lab1.task4/task4 `() 1) `()))
