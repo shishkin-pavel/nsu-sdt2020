73ba103b23d5c165a2e0dee0c7be9ca325c00b8f
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-10-02 22:56:36 +0700

    before format

--- core.clj
+++ core.clj
@@ -24,8 +24,8 @@
 
 ;;(println (my-map #(* %1 %1) '(2 4 3)))
 ;;(println (my-map even? '(2 4 3)))
-(println "my-filter")
-(println (my-filter even? (range 1 10)))
+;;(println "my-filter")
+;;(println (my-filter even? (range 1 10)))
 
 ;console output
 ;my-filter
@@ -34,3 +34,25 @@
 ; Syntax error (ArityException) compiling at (c:\home\koshi8bit\lab1\src\lab1\core.clj:28:1).
 ; Wrong number of args (0) passed to: clojure.lang.PersistentVector
 ; Evaluation of file core.clj failed: class clojure.lang.Compiler$CompilerException
+
+(defn in?
+  [coll elm]
+  (some #(= elm %) coll))
+  ; (not (nil? (some #(= elm %) coll))))
+
+(defn f
+  [data]
+  (println data))
+
+(defn f2
+  [data]
+  (if (empty? data)
+      '()
+      (do
+        (f (first data)
+        (f2 (rest data))
+      ))))
+
+
+
+(f2 '(1 4 3))
