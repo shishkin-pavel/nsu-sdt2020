449b3bc022503f828c305f12d4d3ad24ac6660a1
mephisto <mephisto@skbkontur.ru>
2020-10-02 23:04:44 +0700

    done task_3

---
+++ Test/src/test/task_3.clj
@@ -0,0 +1,14 @@
+(ns test.task_3)
+
+(defn my-map [f coll]
+  (reduce #(conj %1 (f %2)) [] coll))
+
+
+(defn my-filter [cond coll]
+  (if
+    (empty? coll)
+    `()
+    (if
+      (cond (first coll))
+      (cons (first coll) (my-filter cond (rest coll)))
+      (my-filter cond (rest coll)))))
