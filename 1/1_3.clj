fb60fd302318f97e6fb2fb9d9eb42d616d4281fb
pda898 <pda00898@gmail.com>
2020-10-02 14:32:08 +0700

    1.3 with reduce

--- 1/1_3.clj
+++ 1/1_3.clj
@@ -1,15 +1,4 @@
-;; question - is reduce supposed to be used as list reversal?
 (defn my-map
-    ([func in_list] (my-map func in_list 0 (list)))
-    ([func in_list index out_list] 
-        (if (< index (count in_list))
-            (recur func in_list (inc index) (concat out_list [(func (nth in_list index))]))
-            out_list)))
+    [func in_list] (reduce (fn [x y] (concat x [(func y)])) (cons (list) in_list)))
 (defn my-filter
-    ([func in_list] (my-filter func in_list 0 (list)))
-    ([func in_list index out_list] 
-        (if (< index (count in_list))
-            (if (func (nth in_list index))
-                (recur func in_list (inc index) (concat out_list [(nth in_list index)]))
-                (recur func in_list (inc index) out_list))
-            out_list)))
+    [func in_list] (reduce (fn [x y] (if (func y)(concat x [y]) x)) (cons (list) in_list)))
