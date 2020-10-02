934c1ef7597836bfbaf101dc98de48a4b93eb13f
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-10-03 01:47:00 +0700

    1.4 in progress. only str

---
+++ 1-4.clj
@@ -0,0 +1,30 @@
+(ns lab1.core
+  (:gen-class))
+
+(defn in?
+  [coll e]
+  (some #(= e %) coll))
+
+(defn mix
+  [alph lett]
+  (do
+    ;(println alph lett)
+    ;                     kak sdelat #() nado li?
+    (reduce (fn [acc e] (concat (map (fn [x] (concat x e))
+                                (filter (fn [x] (not (in? x e)))
+                                        lett))
+                         acc))
+            (cons '() alph))))
+
+
+(defn permutation
+  [alph, k]
+  (if (<= k 0)
+      '()
+      (reduce (fn [acc i] (mix alph acc))
+              (cons alph (range (dec k))))))
+
+; po4emu tolko so strokami robit
+(println (permutation '("1" "2" "3") 2))
+
+
