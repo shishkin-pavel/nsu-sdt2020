09df3812cad53c18ca9c66c1728feb8628074f2e
pda898 <pda00898@gmail.com>
2020-10-03 10:38:26 +0700

    1.4 with any objects

--- 1/1_4.clj
+++ 1/1_4.clj
@@ -1,5 +1,5 @@
 ;; solution - combine func
 (defn create
-    [alph base] (reduce (fn [res y] (concat (map (fn [x] (.concat x y)) (filter (fn [x] (not (.endsWith x y))) base)) res)) (cons (list) alph)))
+    [alph base] (reduce (fn [res y] (concat (map (fn [x] (if (= (class x) java.lang.String) (concat [x] [y])(concat x [y]))) (filter (fn [x] (if (= (class x) java.lang.String) (not (= x y)) (not (= (last x) y)))) base)) res)) (cons (list) alph)))
 (defn combine
     [alph, n] (reduce (fn [x y] (create alph x)) (cons alph (range 1 n))))
