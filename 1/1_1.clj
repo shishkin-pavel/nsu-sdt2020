84a6e5f4f82938b373ec5c3c48db0cf922be9661
pda898 <pda00898@gmail.com>
2020-10-03 12:01:54 +0700

    1 solved

--- 1/1_1.clj
+++ 1/1_1.clj
@@ -3,22 +3,28 @@
 	([base sym] (foreach base sym 0 (list)))
 	([base sym index res]
 		(if (< index (- (count base) 1))
-            (if (.endsWith (nth base index) sym)
+            (if (= (last (nth base index)) sym)
                 (recur base sym (inc index) res)
-                (recur base sym (inc index) (cons (.concat (nth base index) sym) res)))
-            (if (.endsWith (nth base index) sym)
+                (recur base sym (inc index) (cons (concat (nth base index) [sym]) res)))
+            (if (= (last (nth base index)) sym)
                 res
-                (cons (.concat (nth base index) sym) res)))))
+                (cons (concat (nth base index) [sym]) res)))))
 (defn create 
 	([alph base] (create alph base 0 (list)))
 	([alph base index res]
     (if (< index (count alph))
         (recur alph base (inc index) (concat (foreach base (nth alph index)) res))
-        res)))	
+        res)))    
+(defn prepfirst
+    ([alph] (prepfirst alph 0 (list)))
+    ([alph n res]
+        (if (< n (count alph))
+            (recur alph (inc n) (cons (list (nth alph n)) res))
+            res)))
 (defn combine 
-	([alph, n] (combine alph (dec n) alph))
-	([alph, n, res] 
-	    (if (> n 0) 
+    ([alph, n] (combine alph (dec n) (prepfirst alph)))
+    ([alph, n, res] 
+        (if (> n 0) 
             (recur alph (dec n) (create alph res))
             res
     )))
