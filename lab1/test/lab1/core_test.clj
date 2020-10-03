daac9d803d2bc1b981caaf7608c2bcd0b0f5fa1c
Vlad264 <vlad-498@yandex.ru>
2020-10-03 10:28:17 +0700

    Create tests for other functions

--- lab1/test/lab1/core_test.clj
+++ lab1/test/lab1/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [lab1.core :refer :all]))
 
+(deftest add-char-test
+  (testing "add-char"
+    (is (= (lab1.task1/add-char "" `()) `()))
+    (is (= (lab1.task1/add-char "a" `()) `()))
+    (is (= (lab1.task1/add-char "a" `("a")) `()))
+    (is (= (lab1.task1/add-char "a" `("a" "b")) `("ab")))
+    (is (= (lab1.task1/add-char "a" `("a" "b" "c")) `("ab" "ac")))))
+
+(deftest concat-string-list-test
+  (testing "concat-string-list"
+    (is (= (lab1.task1/concat-string-list `() `()) `()))
+    (is (= (lab1.task1/concat-string-list `("a") `("a")) `()))
+    (is (= (lab1.task1/concat-string-list `("a" "b" "c") `("a" "b" "c")) `("ab" "ac" "ba" "bc" "ca" "cb")))
+    (is (= (lab1.task1/concat-string-list `("a" "b" "c") `("ab" "ac" "ba" "bc" "ca" "cb")) `("aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc")))))
+
 (deftest task1-test
   (testing "task1"
     (is (= (lab1.task1/task1 `() 1) `()))
@@ -9,6 +24,21 @@
     (is (= (lab1.task1/task1 `("a" "b" "c") 1) `("a" "b" "c")))
     (is (= (lab1.task1/task1 `("a" "b" "c") 2) `("ab" "ac" "ba" "bc" "ca" "cb")))
     (is (= (lab1.task1/task1 `("a" "b" "c") 3) `("aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc")))))
+
+(deftest add-char-recur-test
+  (testing "add-char-recur"
+    (is (= (lab1.task2/add-char-recur "" `()) `()))
+    (is (= (lab1.task2/add-char-recur "a" `()) `()))
+    (is (= (lab1.task2/add-char-recur "a" `("a")) `()))
+    (is (= (lab1.task2/add-char-recur "a" `("a" "b")) `("ab")))
+    (is (= (lab1.task2/add-char-recur "a" `("a" "b" "c")) `("ab" "ac")))))
+
+(deftest concat-string-list-recur-test
+  (testing "concat-string-list-recur"
+    (is (= (lab1.task2/concat-string-list-recur `() `()) `()))
+    (is (= (lab1.task2/concat-string-list-recur `("a") `("a")) `()))
+    (is (= (lab1.task2/concat-string-list-recur `("a" "b" "c") `("a" "b" "c")) `("ab" "ac" "ba" "bc" "ca" "cb")))
+    (is (= (lab1.task2/concat-string-list-recur `("a" "b" "c") `("ab" "ac" "ba" "bc" "ca" "cb")) `("aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc")))))
 
 (deftest task2-test
   (testing "task2"
@@ -28,6 +58,29 @@
     (is (= (lab1.task3/my-filter (fn [x] (< x 5)) (range 10)) (filter (fn [x] (< x 5)) (range 10))))
     (is (= (lab1.task3/my-filter (fn [x] (> x 5)) (range 10)) (filter (fn [x] (> x 5)) (range 10))))
     (is (= (lab1.task3/my-filter (fn [x] (= x "a")) `("a" "b" "a")) (filter (fn [x] (= x "a")) `("a" "b" "a"))))))
+
+(deftest filter-first-test
+  (testing "filter-first"
+    (is (= (lab1.task4/filter-first "" `()) `()))
+    (is (= (lab1.task4/filter-first "a" `()) `()))
+    (is (= (lab1.task4/filter-first "a" `("a")) `()))
+    (is (= (lab1.task4/filter-first "a" `("a" "b")) `("b")))
+    (is (= (lab1.task4/filter-first "a" `("a" "b" "c")) `("b" "c")))))
+
+(deftest add-char-map-test
+  (testing "add-char"
+    (is (= (lab1.task4/add-char-map "" `()) `()))
+    (is (= (lab1.task4/add-char-map "a" `()) `()))
+    (is (= (lab1.task4/add-char-map "a" `("a")) `()))
+    (is (= (lab1.task4/add-char-map "a" `("a" "b")) `("ab")))
+    (is (= (lab1.task4/add-char-map "a" `("a" "b" "c")) `("ab" "ac")))))
+
+(deftest concat-string-list-map-test
+  (testing "concat-string-list-map"
+    (is (= (lab1.task4/concat-string-list-map `() `()) `()))
+    (is (= (lab1.task4/concat-string-list-map `("a") `("a")) `()))
+    (is (= (lab1.task4/concat-string-list-map `("a" "b" "c") `("a" "b" "c")) `("ab" "ac" "ba" "bc" "ca" "cb")))
+    (is (= (lab1.task4/concat-string-list-map `("a" "b" "c") `("ab" "ac" "ba" "bc" "ca" "cb")) `("aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc")))))
 
 (deftest task4-test
   (testing "task4"
