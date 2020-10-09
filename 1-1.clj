8ad40586fd292ed1142eab3e7981048b82f6a8e2
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-10-09 21:53:53 +0700

    1.1 f2 err

--- 1-1.clj
+++ 1-1.clj
@@ -2,26 +2,30 @@
   (:gen-class))
 
 (defn f1 [w alph]
-    ;(println w alph)
-    (if (> (count alph) 0)
+    (println "f1" w alph)
+    (if (empty? alph)
+        '()
         (if (not= (first w) (first alph))
             (concat (list (cons (first alph) w))
                     (f1 w (rest alph)))
             (f1 w (rest alph))
         )
-        '()
     )
 )
 (defn f2 [w alph]
-    ;(println w alph)
+    (println "f2" w alph)
     (if (empty? w)
-        []
+        '()
         (concat
-            (f1 w alph)
-            (f2 (rest w) alph))))
+            (f1 (first w) alph)
+            (f2 (rest w) alph)
+        )
+    )
+)
 
 
-(println "f1" (f1 '() '(:a :b :c)))    ;((b a) (c a))
-(println "f1" (f1 '(:a) '(:a :b :c)))    ;((b a) (c a))
-;(println "f1" (f1 '(:a :b) '(:a :b :c))) ;((b a) (c a))
-;(println "f2" (f2 '(:a :b) '(:a :b :c))) ;((b a) (c a) (a b) (c b))
+
+
+(println "f1 a" (f1 '() '(:a :b :c)))      ;((a) (b) (c))
+(println "f1 b" (f1 '(:a) '(:a :b :c)))    ;((b a) (c a))
+(println "f2" (f2 '(:a :b) '(:a :b :c)))   ;((b a) (c a) (a b) (c b))
