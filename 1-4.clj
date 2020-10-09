7db4e671890359ea929a32caaf5b5f60673d89a7
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-10-09 22:05:02 +0700

    1-4 f1

--- 1-4.clj
+++ 1-4.clj
@@ -1,32 +1,12 @@
 (ns lab1.core
   (:gen-class))
+  (defn f1
+    [w alph]
+    (map
+        #(cons % w)
+        (filter #(not= (first w) %) alph)
+    )
+)
 
-(defn in?
-  [coll e]
-  (some #(= e %) coll))
-
-(defn mix
-  [alph lett]
-  (do
-    ;(println alph lett)
-    ;                     kak sdelat #() nado li?
-    (reduce (fn [acc e] (concat (map (fn [x] (concat x e))
-                                (filter (fn [x] (not (in? x e)))
-                                        lett))
-                         acc))
-            (cons '() alph))))
-
-
-(defn permutation
-  [alph, k]
-  (if (<= k 0)
-      '()
-      (reduce (fn [acc i] (mix alph acc))
-              (cons alph (range (dec k))))))
-
-; po4emu tolko so strokami robit
-(println (permutation '("1" "2" "3") 2))
-
-(#(println %1 %&) '(1 2 3) '(4 5) '(6 7))
-
-
+(println (f1 `() `(:a :b :c)))
+(println (f1 `(:a) `(:a :b :c)))
