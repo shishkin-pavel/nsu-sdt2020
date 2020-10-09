baea5f25caeccc458967ed49fce8f7492d1c29e3
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-10-09 21:37:36 +0700

    f1 ok

--- 1-1.clj
+++ 1-1.clj
@@ -1,16 +1,17 @@
 (ns lab1.core
   (:gen-class))
 
-
 (defn f1 [w alph]
     ;(println w alph)
-    (if (empty? alph)
-        '()
+    (if (> (count alph) 0)
         (if (not= (first w) (first alph))
             (concat (list (cons (first alph) w))
-                    (f1 (rest alph) w))
-            (f1 w (rest alph)))))
-
+                    (f1 w (rest alph)))
+            (f1 w (rest alph))
+        )
+        '()
+    )
+)
 (defn f2 [w alph]
     ;(println w alph)
     (if (empty? w)
@@ -20,27 +21,7 @@
             (f2 (rest w) alph))))
 
 
-;---
-
-(defn lab0 [word lst]
-  (if (empty? lst) []
-      (if (= (str (nth (first word) 0)) (str (first lst)))
-        (lab0 word (rest lst))
-        (cons (cons (first lst) (first word)) (lab0 word (rest lst))))))
-
-(defn lab01
-  ([word lst]
-   (if (empty? word)
-     []
-     (concat (lab0 word lst) (lab01 (rest word) lst)))))
-
-;---
-
 (println "f1" (f1 '() '(:a :b :c)))    ;((b a) (c a))
 (println "f1" (f1 '(:a) '(:a :b :c)))    ;((b a) (c a))
 ;(println "f1" (f1 '(:a :b) '(:a :b :c))) ;((b a) (c a))
 ;(println "f2" (f2 '(:a :b) '(:a :b :c))) ;((b a) (c a) (a b) (c b))
-
-;(println "Antoha f1" (lab0 '("a") '("a" "b" "c")))
-;(println "Antoha f1" (lab0 '("a" "b") '("a" "b" "c")))
-;(println "Antoha f2" (lab01 '("a" "b") '("a" "b" "c")))
