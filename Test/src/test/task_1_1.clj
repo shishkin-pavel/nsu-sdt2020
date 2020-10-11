a04f0c43193958b215aed3cbd36832e5e0bdaec7
mephisto <mephisto@skbkontur.ru>
2020-10-11 18:58:45 +0700

    refactor

--- Test/src/test/task_1_1.clj
+++ Test/src/test/task_1_1.clj
@@ -1,34 +1,24 @@
 (ns test.task_1_1)
 
-(defn get-pair [sym alph res]
+(defn get-pair [word alph]
   (if (= 0 (count alph))
-    res
-    (if (.contains (str sym) (str (first alph)))
-      (get-pair sym (rest alph) res)
-      (get-pair sym (rest alph) (cons (cons sym (list (first alph))) res)))))
+    ()
+    (if (= (first word) (first alph))
+      (get-pair word (rest alph))
+      (cons (cons (first alph) word) (get-pair word (rest alph))))))
 
-(defn mix [alph tmp res]
-  (if (= 0 (count tmp))
-    res
-    (mix alph (rest tmp) (concat (get-pair (first tmp) alph `()) res))))
+(defn mix [alph words]
+  (if (= 0 (count words))
+    ()
+    (concat (mix alph (rest words)) (get-pair (first words) alph))))
 
-(defn repeat-n-times [alph n res]
-  (if (= 1 n)
-    res
-    (repeat-n-times alph (dec n) (mix alph res `()))))
-
-(defn remove-braces-for-element [res n]
-  (if (= n 2)
-    res
-    (remove-braces-for-element (concat (first res) (rest res)) (dec n))))
-
-(defn remove-braces [permuts n res]
-  (if (= 0 (count permuts))
-    res
-    (remove-braces (rest permuts) n (concat (list (remove-braces-for-element (first permuts) n)) res))))
+(defn repeat-n-times [alph n]
+  (if (= 0 n)
+    `(())
+    (mix alph (repeat-n-times alph (dec n)))))
 
 (defn my-permut [alph n]
-   (remove-braces (repeat-n-times alph n alph) n `()))
+  (repeat-n-times alph n))
 
 (defn -main [& _]
-  (prn (my-permut `(:a (:b 1) "c") 3)))
+  (prn (my-permut `(:a :b "c") 3)))
--- Test/src/test/task_1_2.clj
+++ Test/src/test/task_1_2.clj
@@ -1,35 +1,24 @@
 (ns test.task_1_2)
 
-(defn get-pair [sym alph res]
-  (if (= 0 (count alph))
-    res
-    (if (.contains (str sym) (str (first alph)))
-      (recur sym (rest alph) res)
-      (recur sym (rest alph) (cons (cons sym (list (first alph))) res)))))
+(defn get-pair [word alph]
+  (loop [alph alph res ()]
+    (if (= 0 (count alph))
+      res
+      (if (= (first word) (first alph))
+        (recur (rest alph) res)
+        (recur (rest alph) (cons (cons (first alph) word) res))))))
 
-(defn mix [alph tmp res]
-  (if (= 0 (count tmp))
-    res
-    (recur alph (rest tmp) (concat (get-pair (first tmp) alph `()) res))))
-
-(defn repeat-n-times [alph n res]
-  (if (= 1 n)
-    res
-    (recur alph (dec n) (mix alph res `()))))
-
-(defn remove-braces-for-element [res n]
-  (if (= n 2)
-    res
-    (recur (concat (first res) (rest res)) (dec n))))
-
-(defn remove-braces [permuts n res]
-  (if (= 0 (count permuts))
-    res
-    (recur (rest permuts) n (concat (list (remove-braces-for-element (first permuts) n)) res))))
+(defn mix [alph words]
+  (loop [words words res ()]
+    (if (= 0 (count words))
+      res
+      (recur (rest words) (concat (get-pair (first words) alph) res)))))
 
 (defn my-permut [alph n]
-  (remove-braces (repeat-n-times alph n alph) n `()))
-
+  (loop [n n res `(())]
+    (if (= 0 n)
+      res
+      (recur (dec n) (mix alph res)))))
 
 (defn -main [& _]
-  (prn (my-permut `(:a (:b 1) "c") 3)))
+  (prn (my-permut `(:a :b "c") 3)))
