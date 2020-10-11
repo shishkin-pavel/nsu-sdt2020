aa4537f2001dd54f4c30ae3064847ec0c6772ee6
mephisto <mephisto@skbkontur.ru>
2020-10-11 20:45:22 +0700

    my-filter by reduce

--- Test/src/test/task_1_3.clj
+++ Test/src/test/task_1_3.clj
@@ -3,12 +3,5 @@
 (defn my-map [f coll]
   (reduce #(conj %1 (f %2)) [] coll))
 
-
 (defn my-filter [cond coll]
-  (if
-    (empty? coll)
-    `()
-    (if
-      (cond (first coll))
-      (cons (first coll) (my-filter cond (rest coll)))
-      (my-filter cond (rest coll)))))
+  (reduce #(if (cond %2) (concat %1 [%2]) %1) [] coll))
