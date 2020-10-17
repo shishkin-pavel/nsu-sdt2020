601b1457c0e2e396cc0e2d0aef94d4056a4a9238
pda898 <pda00898@gmail.com>
2020-10-17 12:30:29 +0700

    lazy fixed

--- 2/integral_lazy.clj
+++ 2/integral_lazy.clj
@@ -2,13 +2,14 @@
 (ns integral-lazy
     (:gen-class))
 (defn integral [func step]
+    (let 
+        [sum (map first (iterate (fn [[s i]] [(+ s (/ (* (+ (func i) (func (- i step))) step) 2)) (+ i step)]) [0.0 step]))
+        negsum (map first (iterate (fn [[s i]] [(+ s (/ (* (+ (func i) (func (+ i step))) step) 2)) (- i step)]) [0.0 (* -1 step)]))]
     (fn [b] 
     (if (> b 0)
         (let 
-            [diff (- b (* (Math/floor (/ b step)) step))
-             sum (map first (iterate (fn [[s i]] [(+ s (/ (* (+ (func i) (func (- i step))) step) 2)) (+ i step)]) [0.0 step]))] 
+            [diff (- b (* (Math/floor (/ b step)) step))] 
              (+ (nth sum (Math/floor (/ b step))) (/ (* (+ (func b) (func (- b diff))) diff) 2)))
         (let 
-            [diff (- b (* (Math/floor (/ b step)) step))
-            negsum (map first (iterate (fn [[s i]] [(+ s (/ (* (+ (func i) (func (+ i step))) step) 2)) (- i step)]) [0.0 (* -1 step)]))] 
-            (* (+ (nth negsum (Math/floor (/ (* -1 b) step))) (/ (* (+ (func b) (func (+ b diff))) diff) 2)) -1)))))
+            [diff (- b (* (Math/floor (/ b step)) step))] 
+            (* (+ (nth negsum (Math/floor (/ (* -1 b) step))) (/ (* (+ (func b) (func (+ b diff))) diff) 2)) -1))))))
