f675144d2e14ef1c7e45efd889dce96283f879c1
Anaksimander <61375814+Anaksimander@users.noreply.github.com>
2020-10-18 19:13:12 +0700

    Create laba2_1.clj

---
+++ laba2/laba2_1.clj
@@ -0,0 +1,24 @@
+(ns laba2-1.core
+  (:gen-class))
+
+(defn mini [fun x0 x1 dx]
+  (* (* (+ (fun x0) (fun x1)) 0.5) dx))
+
+
+(defn integral [fun x1 dx]
+  (if (not= x1 0)
+    (let [x0 (- x1 dx)] (+ (mini fun x0 x1 dx)) (integral fun x0 dx))
+    0))
+
+(def integral-m (memoize integral))
+
+(def f #(* % %))
+(defn -main []
+  (time (integral f 100 1))
+  (println "---------1")
+  (time (integral-m f 100 1))
+  (time (integral f 100 1))
+  (println "--------2")
+  (time (integral f 120 1))
+  (time (integral f 150 1)))
+(-main)
