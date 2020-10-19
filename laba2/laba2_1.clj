9eaca73ebfe691b343488ebabe3a73296e825d04
Anaksimander <61375814+Anaksimander@users.noreply.github.com>
2020-10-19 10:08:02 +0700

    Update laba2_1.clj

--- laba2/laba2_1.clj
+++ laba2/laba2_1.clj
@@ -6,19 +6,19 @@
 
 
 (defn integral [fun x1 dx]
-  (if (not= x1 0)
-    (let [x0 (- x1 dx)] (+ (mini fun x0 x1 dx)) (integral fun x0 dx))
-    0))
+  (let [x0 (- x1 dx)]
+   (if (not= x1 0) (+ (mini fun x0 x1 dx) (integral fun x0 dx))
+    (mini fun x0 x1 dx))
+    )
+  )
 
 (def integral-m (memoize integral))
 
 (def f #(* % %))
 (defn -main []
-  (time (integral f 100 1))
+  (time (integral f 1000 1))
   (println "---------1")
-  (time (integral-m f 100 1))
-  (time (integral f 100 1))
-  (println "--------2")
-  (time (integral f 120 1))
-  (time (integral f 150 1)))
+  (time (integral-m f 1000 1))
+  (time (integral-m f 1000 1))
+  (time (integral-m f 1200 1)))
 (-main)
