1d89f4c20979b10f4f4ae5d1b051edd5be9336ae
Anaksimander <61375814+Anaksimander@users.noreply.github.com>
2020-10-20 01:09:15 +0700

    Update laba1_1.clj

--- laba1/laba1_1.clj
+++ laba1/laba1_1.clj
@@ -1,35 +1,38 @@
 (ns laba1-1.core
   (:gen-class))
 
-(defn make_list [word alf]                  ;k - длинна алфавита n- счетчик перечисления алф
+(defn make_list [word alf]          
   (let [num (count alf)]
     (if (= (first word) (first alf)) 
-      (if (> num 1) (make_list word (rest alf)))
-      
+      (if (> num 1) (make_list word (rest alf))
+          alf)
       (let [search (cons (first alf) word)]
         (if (= num 1) (list search)
             (cons search (make_list word (rest alf)))))))
   )
 
-;(make_list '() '(:a :b :c) 2) 
+;(make_list '() '(:a :b :c)) 
 
-(defn multi_make_list [words alf];n_words-начиная с конца-1 до 0  n- счетчик перечисления алф
+(defn multi_make_list [words alf];
     (if (not= (count words) 1) (concat (make_list (first words) alf) (multi_make_list (rest words) alf))
       (make_list (first words) alf)))
   
 
-;(multi_make_list (list '()) '(:a :b :c) 2 0); 
+;(multi_make_list '(()) '(:a :b :c))
 
-(defn mix [words width alf count_width ];count_width=1
-  (if (= count_width 1) (let [zamena (multi_make_list words alf)] (mix zamena width alf (+ count_width 1)))
-      (if (not= count_width width)
-        (let [zamena (multi_make_list words alf)]
-          (mix zamena width alf (+ count_width 1)))
-        (multi_make_list words alf))))
+(defn mix [words width alf count_width];count_width=1
+  (let [zamena (multi_make_list words alf)]
+    (if (= count_width 1) (mix zamena width alf (+ count_width 1))
+        (if (>= count_width width)  (multi_make_list words alf)
+            (mix zamena width alf (+ count_width 1))
+        ))))
 
-;(mix (list '()) 3 '(:a :b :c) 1 2); итоговая функция   (слово ширина_последовательностей алф счетчик_ширины счетчик_алфовита)
+;(mix (list '()) 3 '(:a :b :c) 1); 
 
 (defn main [width alf]
-  (mix '(()) width alf 1))
+  (if (= (count alf) 0) (println "errore")
+    (mix '(()) width alf 1))
+  )
 
-(main 2 '(:a :b :c))
+(main 2 '())
+
