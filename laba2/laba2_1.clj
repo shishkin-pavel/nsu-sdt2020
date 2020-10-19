f16d3fd7c8b0d137a87c45bd935763653caa5dac
Anaksimander <61375814+Anaksimander@users.noreply.github.com>
2020-10-19 10:19:02 +0700

    Update laba2_1.clj

--- laba2/laba2_1.clj
+++ laba2/laba2_1.clj
@@ -1,34 +1,34 @@
 (ns laba2-1.core
   (:gen-class))
 
-
 (defn mini [fun x0 x1 dx]
   (* (* (+ (fun x0) (fun x1)) 0.5) dx))
 
 
 (defn integral [fun x1 dx]
   (let [x0 (- x1 dx)]
-   (if (not= x1 0) (+ (mini fun x0 x1 dx) (integral fun x0 dx))
-    (mini fun x0 x1 dx))
-    )
-  )
+    (if (not= x1 0) (+ (mini fun x0 x1 dx) (integral-m fun x0 dx))
+        (mini fun x0 x1 dx))))
 
 (def integral-m (memoize integral))
 
 (def f #(* % %))
 (defn -main []
-  (time (integral f 1000 1))
-  (println "---------1")
-<<<<<<< HEAD
-  (time (integral-m f 1000 1))
-  (time (integral-m f 1000 1))
-  (time (integral-m f 1200 1)))
-(-main)
-=======
-  (time (integral-m f 100 1))
+  (print "num:100 -")
   (time (integral f 100 1))
-  (println "--------2")
-  (time (integral f 120 1))
-  (time (integral f 150 1)))
+  (println "---------")
+  (print "mem num:100 -")
+  (time (integral-m f 100 1))
+  (print "mem num:100 -")
+  (time (integral-m f 110 1))
+  (print "mem num:1100 -")
+  (time (integral-m f 100 1))
+  (print "mem num:1200 -")
+  (time (integral-m f 120 1))
+  (print "mem num:1300 -")
+  (time (integral-m f 130 1))
+  (print "mem num:1400 -")
+  (time (integral-m f 140 1))
+  (print "mem num:1500 -")
+  (time (integral-m f 150 1)))
 (-main)
->>>>>>> d55767b9dcb9e0d23598cce7886f52aa02ca742c
