cb48ac725e4a185abed355bcd66995f583bc0933
Anaksimander <61375814+Anaksimander@users.noreply.github.com>
2020-10-20 13:24:46 +0700

    Create laba2_2 - reductions.clj

---
+++ laba2/laba2_2 - reductions.clj
@@ -0,0 +1,34 @@
+(ns laba2-2.core
+  (:gen-class))
+
+
+;; (defn trapec [fun dx]
+;;   (map first (iterate
+;;        (fn [[sum x]] [(+ sum (* (* (+ (fun (- x dx)) (fun x)) 0.5) dx)) (+ x dx)]) [0.0 dx])))
+
+(defn trapec [fun a dx]
+  (reductions (fn [x] + (* (* (+ (fun x) (fun (+ x dx))) 0.5) dx) )  (range 0.0 (+ a dx) dx)
+              ))
+
+
+(defn integral [fun x dx]
+  (nth (trapec fun x dx) (- (/ x dx) 1)))
+
+
+(def f #(* % %))
+(def dx 1)
+(defn -main []
+  (print "num:100 -")
+  (time (integral f 100 dx))
+  (print "num:100 -")
+  (time (integral f 100 dx))
+  (print "num:110 -")
+  (time (integral f 110 dx))
+  (print "num:120 -")
+  (time (integral f 120 dx))
+  (print "num:130 -")
+  (time (integral f 130 dx))
+  (print "num:140 -")
+  (time (integral f 140 dx))
+  )
+(-main)
