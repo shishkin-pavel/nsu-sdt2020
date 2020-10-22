1ba0ea46237d336814823715cff75f1c5879fa0d
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-10-22 22:52:03 +0700

    2.2 cleared

--- 2-2.clj
+++ 2-2.clj
@@ -5,25 +5,6 @@
     ;(println "calc-trap" a b)
   (* (/ (+ (f a) (f b)) 2) (- b a)))
 
-
-(let [maxx 10 delta 2]
-
-    (take (/ maxx delta)
-        (iterate #(+ % delta) 0)
-    )
-)
-
-(let [step 5 begin 10]
-(let [ls (iterate #(+ % step) begin)]
-    (take 10 ls)
-)
-)
-
-
-(println ((fn [x] 1) 2))
-(println ((fn [x] 1) 100))
-
-
 (defn get-integrator [f begin step]
     (let [ls (iterate #(+ % (calc-trap f % (+ % step)))
                       begin)]
@@ -34,20 +15,8 @@
     )
 )
 
-(let [integrator (get-integrator (fn [x] 2) 0 1)]
-    (time (integrator 10))
-    (time (integrator 10))
+(let [integrator (get-integrator (fn [x] 1) 0 1)]
+    (time (integrator 10000))
+    (time (integrator 10015))
+    (time (integrator  9985))
 )
-
-
-;;; Evaluating file: core.clj
-;;"Elapsed time: 0.0376 msecs"
-;;"Elapsed time: 0.0054 msecs"
-;;(0 1/2 3/2 7/2 15/2 31/2 63/2 127/2 255/2 511/2)
-
-
-;;(let [integrator (get-integrator #(* % 1) 0 5)]
-;;    (time (integrator 10000))
-;;    (time (integrator 10015))
-;;    (time (integrator 09985))
-;;)
