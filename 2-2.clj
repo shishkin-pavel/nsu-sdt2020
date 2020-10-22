e25b8e114574d77ce9c472f3eb522f88465a5922
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-10-22 23:10:37 +0700

    reductions example

--- 2-2.clj
+++ 2-2.clj
@@ -2,15 +2,29 @@
   (:gen-class))
 
 (defn calc-trap [f a b]
-    ;(println "calc-trap" a b)
   (* (/ (+ (f a) (f b)) 2) (- b a)))
+
+;;--------------
+;; better way
+(let [seqq (map #(* 2 %) (range))]
+    (->>
+        (map
+            #(calc-trap (fn [x] 1) %1 %2)
+            seqq
+            (rest seqq)
+        )
+        (reductions + 0)
+        (take 10)
+    )
+)
+
+;;--------------
 
 (defn get-integrator [f begin step]
     (let [ls (iterate #(+ % (calc-trap f % (+ % step)))
                       begin)]
         (fn [k]
             (nth ls (/ k step))
-            ;(take (/ k step) ls)
         )
     )
 )
@@ -20,3 +34,4 @@
     (time (integrator 10015))
     (time (integrator  9985))
 )
+
