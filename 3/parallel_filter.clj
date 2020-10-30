8d4d0200c36ba47f743fa60e1fff39b89075f465
pda898 <pda00898@gmail.com>
2020-10-30 21:27:09 +0700

    fixed 1 thread only error

--- 3/parallel_filter.clj
+++ 3/parallel_filter.clj
@@ -3,4 +3,11 @@
 (defn parallel-filter
     ([pred coll] (parallel-filter pred coll (+ 2 (.. Runtime getRuntime availableProcessors))))
     ([pred coll thread_count] 
-        (reduce concat (map deref (doall (map #(future (filter pred %)) (partition-all (Math/ceil (/ (count coll) thread_count)) coll)))))))
+        (->> coll
+        (partition-all (Math/ceil (/ (count coll) thread_count)))
+        (map #(future (doall (filter pred %))))
+        (doall)
+        (map deref)
+        (reduce concat)
+        )))
+        ; (reduce concat (map deref (doall (map #(future (doall (filter pred %))) (partition-all (Math/ceil (/ (count coll) thread_count)) coll)))))))
